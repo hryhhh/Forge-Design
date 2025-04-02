1a198ef58aa5d76ad57257265508e366a7f0f5bb
Hry-hhh <13760744+hry-hhh@user.noreply.gitee.com>
2025-04-02 21:34:13 +0800

    feat(eslint): 更新 ESLint 配置并添加 React 支持
    - 更新 ESLint 配置文件，增加对 TypeScript 和 React 的支持
    - 添加全局声明文件 global.d.ts，声明 SCSS 模块类型定义
    - 更新项目依赖，包括 React、ESLint 插件和 TypeScript

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,34 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import pluginReact from "eslint-plugin-react";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+      plugins: ["@typescript-eslint", "react"],
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react/recommended",
+    ],
+  },
+  js.configs.recommended,
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ]);
