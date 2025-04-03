ab06b6b6e1a3b13a5321739a81adbef18fc46cf2
Hry-hhh <13760744+hry-hhh@user.noreply.gitee.com>
2025-04-03 22:27:46 +0800

    Button styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import { useState } from 'react'
-import Button,{ ButtonSize, ButtonType } from './components/Button'
+//import React from "react";
+import Button, { ButtonSize, ButtonType } from "./components/Button/Button";
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        
         <Button>Hello Button</Button>
-        <Button size={ButtonSize.Large}>Large Button</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Button</Button>
+        <Button btnType={ButtonType.Danger}>Danger Button</Button>
+        <Button btnType={ButtonType.Link} href="https://www.baidu.com">Link Button</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <a   
-        href="https://vitejs.dev/guide/features.html"
-        target="_blank"
-      >learn react</a>
-      
+      <a href="https://vitejs.dev/guide/features.html" target="_blank">
+        learn react
+      </a>
     </>
-  )
+  );
 }
 
 export default App
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import classNames from 'classnames';
+import React from "react";
+import classNames from "classnames";
 
 export enum ButtonSize {
-  Small = 'small',
-  Medium = 'medium',
-  Large = 'large',
+  Small = "small",
+  Medium = "medium",
+  Large = "large",
 }
 
 export enum ButtonType {
-  Primary = 'primary',
-  Default = 'default',
-  Danger = 'danger',
-  Link = 'link',
+  Primary = "primary",
+  Default = "default",
+  Danger = "danger",
+  Link = "link",
 }
 
 // 定义默认属性
@@ -20,29 +20,24 @@ interface BaseButtonProps {
   size?: ButtonSize;
   btnType?: ButtonType;
   disabled?: boolean;
-  className?: string; // 修正为 string 类型
+  className?: string;
   href?: string;
 }
 
-// 设置默认值
-const defaultProps: BaseButtonProps = {
-  size: ButtonSize.Medium,
-  btnType: ButtonType.Primary,
-  disabled: false,
-};
+
 
 const Button: React.FC<BaseButtonProps> = ({
   children,
-  size = defaultProps.size,
-  btnType = defaultProps.btnType,
-  disabled = defaultProps.disabled,
-  className,
+  size = ButtonSize.Medium,
+  btnType = ButtonType.Primary,
+  disabled=false,
+   className,
   href,
 }) => {
-  const classes = classNames('btn', {
+  const classes = classNames("btn", {
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
-    'disabled': btnType === ButtonType.Link && disabled,
+    disabled: btnType === ButtonType.Link && disabled,
   });
 
   if (btnType === ButtonType.Link && href) {
